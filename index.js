6e68bba305debcdaa9e9321af14814210dc3c3c4
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-29 23:09:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution here
+
+function findMatching(driversArray, str) {
+    return driversArray.filter((driver) => {
+        return driver.toLowerCase() == str.toLowerCase();
+    });
+};
+
+let drivers = ['Racquel', 'Izzy', 'Ahava'];
+console.log(findMatching(drivers, 'Racquel'));
+
+function fuzzyMatch(driversArray, str) {
+    return driversArray.filter((driver) => {
+        return driver.slice(0, str.length) === str;
+    })
+};
+
+console.log(fuzzyMatch(drivers, 'Ra'));
+
+let driverObjects = [
+    {
+        name: 'Racquel',
+        hometown: 'Woodmere',
+    },
+    {
+        name: 'Izzy',
+        hometown: 'Woodmere',
+    },
+    {
+        name: 'Gil',
+        hometown: 'St. Louis',
+    },
+]
+
+function matchName(driversArray, str) {
+    return driversArray.filter((driver) => {
+        return driver.name === str;
+    });
+};
+
+console.log(matchName(driverObjects, 'Gil'));
